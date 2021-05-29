bb09f712c1807a5b7097acea42739aee85c2f6f3
Nicolae Teslaru <teslaruzero@gmail.com>
2021-05-29 13:23:35 +0200

    Add Translations

--- page-newsletter.php
+++ page-newsletter.php
@@ -6,7 +6,7 @@
 
     <div class="container-fluid bg-primary text-white text-center newsletter-message-container flex-column">
       <div class="w-md-75"><?php the_content(); ?></div>
-      <a href="<?php echo get_site_url(); ?>" class="btn btn-light my-4">Home</a>
+      <a href="<?php echo get_site_url(); ?>" class="btn btn-light my-4"><?php __('Home', 'ybp') ?></a>
     </div>
 
 <?php
