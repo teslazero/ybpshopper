b36980e41e4021f9861975005224458c8cd7fb9e
Nicolae Teslaru <teslaruzero@gmail.com>
2021-05-29 14:57:43 +0200

    Fix language switch

--- header.php
+++ header.php
@@ -78,6 +78,11 @@ M516.199282 202.933296 C517.941348 206.024604 520.748660 208.923530 520.627016 2
           <i class="fab fa-facebook-f"></i>
         </a>
       </li>
+        <style>
+            .language-switch::after {
+                content: '' !important;
+            }
+        </style>
         <li class="nav-item dropdown hovered ml-n4">
           <?php
             $languages = icl_get_languages('skip_missing=0&orderby=code');
@@ -85,7 +90,7 @@ M516.199282 202.933296 C517.941348 206.024604 520.748660 208.923530 520.627016 2
             if(!empty($languages)){
               foreach($languages as $l){
                 if($l['active']) {
-                  echo '<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="true"><img class="mb-1 mr-1" src="'.$l['country_flag_url'].'"></a>';
+                  echo '<a class="nav-link dropdown-toggle language-switch" data-toggle="dropdown" href="#" aria-expanded="true"><img class="mb-1 mr-1" src="'.$l['country_flag_url'].'"></a>';
                 }
               }
             }
